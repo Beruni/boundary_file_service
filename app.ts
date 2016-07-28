fdc7d22eedee6752dfb12dca4e4b41c3a263a212
Souvik Das <souvikd@thoughtworks.com>
2016-07-28 12:18:36 +0530

    | #3 | Souvik + Praveer | Store file directly mongoDb as file chunks
    - Add GridFs

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as multer from "multer";
+import * as mongoose from 'mongoose';
+import * as gridfs from 'gridfs-stream';
+import * as fs from 'fs';
 
 var app = express();
 
 app.set('port', process.env.PORT || '3000');
+app.set('mongo_host', process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost');
+
 app.use(bodyParser.json());
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -25,9 +30,17 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
 
 var uploadConfig = multer({ dest: "./uploads" });
 
-app.post("/upload", uploadConfig.single('upl'), (req: express.Request, res: express.Response) => {
-    console.log(req.file);
-    res.status(204).end();
+app.post("/upload", uploadConfig.single('boundaryFile'), (req: express.Request, res: express.Response) => {
+    var gfs = gridfs(mongoose.connection.db, mongoose.mongo);
+    var writeStream = gfs.createWriteStream();
+
+    fs.createReadStream(req.file.path).pipe(writeStream);
+
+    writeStream.on('close', file => res.status(200).send({fileId: file._id}));
+
+    writeStream.on('error', e => res.status(500).send("Could not upload file"));
 });
+
+mongoose.connect('mongodb://'+ app.get('mongo_host') +'/beruni_boundary_files');
 
 app.listen(app.get('port'));
